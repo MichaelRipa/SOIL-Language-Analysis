8cb1bba47429e9faddbcdf73740d1d11f09bf7a6
MichaelRipa <m.ripa123@gmail.com>
2022-04-10 17:48:09 -0400

    Updated the scrape_articles function to iterate through tags rather then searching by tag type (the latter not working well on certain webpages)

--- SOILKit/scraper.py
+++ SOILKit/scraper.py
@@ -1,49 +1,126 @@
 #! /usr/bin/env python3
 
-from random import randint
+'''scraper.py - Contains code for scraper used (specifically) for obtaining newspaper articles from "La repubblica" website '''
+
+from random import randint,random,shuffle
 import requests
 import bs4
+from time import sleep
 
 URL = 'https://ricerca.repubblica.it/repubblica/archivio/repubblica/'
 
 class Scraper:
 
-    def generate_random_url(min_year=2000,max_year=2020):
+    def generate_random_url(n=None,min_year=2000,max_year=2020):
+        '''Randomly returns n urls with dates that fall between min_year and max_year. By default, the function returns all possible dates'''
 
         new_url = URL
-        year = str(randint(min_year,max_year))
-        month = str(randint(1,12))
-        day = str(randint(1,28))
+        urls = []
+        for year in range(min_year,max_year+1):
+            for month in range(1,13):
+                for day in range(1,29):
+                    urls.append(new_url + str(year) + '/' + str(month) + '/' + str(day))
 
-        if len(month) < 2:
-            month = '0' + month
+        shuffle(urls)
+        if n == None:
+            return urls
 
-        if len(day) < 2:
-            day = '0' + day
+        return urls[0:n]
 
-        return new_url + year + '/' + month + '/' + day
+
+    def _grab_article_link(article):
+        
+        header = ''
+        
+        #This method of iterating through the articles children is more robust, as article.get() does not work on certain pages
+
+        for tag in article.children: 
+            if tag.name == 'h1':
+                header = tag
+            elif tag.name == 'p':
+                if tag.get('span') != None: 
+                    return [] # Filters out non-text media articles 
+
+        if header != '':
+            for tag in header.children:
+                if tag.name == 'a':
+                    if tag.get('title') == "Leggi l'articolo":
+                        return [tag.get('href')] 
+
+        return [] # Couldn't find the hyperlink
+
+
 
 
     def scrape_articles(url=None):
-                
+        '''For a given url of a specific days news articles, returns allhyperlinks on the page that link to (text based) articles'''                
         if url == None:
             url = Scraper.generate_random_url()
 
         req = requests.get(url)
 
-        assert req == 200
+        assert req.status_code == 200
         
         soup = bs4.BeautifulSoup(req.text,'html.parser')
         links = []
 
         for article in soup.find_all('article'):
-            # Will need to check that current article links to written news
-            for link in article.find_all('a'):
-                if link.get('title') == "Leggi l'articolo": 
-                    
-                    links.append(link.get('href'))
+            links += Scraper._grab_article_link(article)
+
+        return links
+            
+    def scrape_page(url=None):
+        '''For a provided url to a news article, scrapes all text obtained from the page'''
+
+        assert url != None
+
+        req = requests.get(url)
+
+        assert req.status_code == 200
+        
+        soup = bs4.BeautifulSoup(req.text,'html.parser')
+
+        article_text = ''
+
+        for div in soup.find_all('div'):
+
+            if div.get('class') != None:
+                if 'story__text' in div.get('class'):
+
+                    #Assume empty article if text too short 
+                    if len(div.text) > 50:
+                        article_text += div.text
+
+        return article_text
+
+        
+    
+    def scrape_pipeline(n=10):
+        ''' For an inputted input n, returns text scrapped from 10 valid articles'''
+        
+        assert type(n) == int
+        assert n > 0
+
+        remaining_articles = n
+        raw_text = ''
+        urls = generate_random_url()
+        i = 0
+        
+        while remaining_articles > 0:
+            
+            # Grab article hyperlinks for arbitrary date
+            new_articles = scrape_articles(urls[i])
+            i += 1
+            for article in new_articles:
+                sleep(random()) # Avoid overloading the server
+                article_text = scrape_article(article)
+                if article_text != '':
+                    raw_text += ' ' + article_text 
+                    remaining_articles -= 1
 
         
         
+        return raw_text
+
 
          
