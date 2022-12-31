4abe8d665948183740469d5f03a255479170ab4b
MichaelRipa <m.ripa123@gmail.com>
2022-12-31 10:56:35 -0500

    Number of small updates, including changing phoneme transcriptions to include ligatures and changes to how Vadar corpus is loaded

--- SOILKit/lexicon.py
+++ SOILKit/lexicon.py
@@ -1,6 +1,9 @@
 #! /usr/bin/env python3
 
 import epitran
+import re
+import operator
+from copy import deepcopy
 
 class Lexicon:
 
@@ -8,23 +11,85 @@ class Lexicon:
         '''Lexicon()
         Allows for retrival and simple phonetic analyis of a lexicon.
         '''
-        self.epi = epitran.Epitran('eng-Latn')
-        self.lex = []
+        self.epi = epitran.Epitran('eng-Latn',ligatures=True)
+        self.lexicon = []
 
-    def load_lexicon(self,path):
-        pass
+    def load_lexicon(self,func,*args):
+        self.lexicon = func(*args)
 
     def lexicon_to_ipa(self):
         '''lexicon_to_ipa()
         Uses epitran to transcribe each word in existing lexicon to ipa, and stores the result in `lex_ipa`. 
         '''
-        self.lex_ipa = []
-        for word in self.lex:
-            ipa = self.epi.transliterate(word)
-            self.lex_ipa.append(ipa)
+        self.lex_ipa = deepcopy(self.lexicon)
+        #This takes roughly 1 minute, English transliteration is extremely slow
+        for entry in self.lex_ipa:
+            entry[0] = self.epi.transliterate(entry[0])
+
+    def partition(self):
+        #TODO: Better design
+        assert hasattr(self,'lexicon') and hasattr(self,'lex_ipa')
+        
+        # This is a little bit messy, probably a more clean way of doing this
+        sentiment = ['pos' if float(entry[1]) > 0 else 'neg' for entry in self.lexicon]
+        positive = []
+        positive_ipa = []
+        negative = []
+        negative_ipa = []
+
+        for i in range(len(self.lexicon)):
+            if sentiment[i] == 'pos':
+                positive.append(self.lexicon[i])
+                positive_ipa.append(self.lex_ipa[i])
+            else:
+                negative.append(self.lexicon[i])
+                negative_ipa.append(self.lex_ipa[i])
+
+        lex_pos = Lexicon()
+        lex_pos.lexicon = positive
+        lex_pos.lex_ipa = positive_ipa
+        lex_neg = Lexicon()
+        lex_neg.lexicon = negative
+        lex_neg.lex_ipa = negative_ipa
+        
+        return lex_pos , lex_neg
+
+    def create_histogram(self,normalize=True):
+        histogram = {}
+        for entry in self.lex_ipa:
+            word = entry[0]
+            for char in word:
+                if char in histogram.keys():
+                    histogram[char] += 1
+                else:
+                    histogram[char] = 1
+        return dict(
+                sorted(
+                    histogram.items(),key=operator.itemgetter(1),reverse=True
+                    )
+                )
 
 
-    def create_histogram(self):
-        pass
 
+#TODO: Document better
+def vadar_loader(*args):
+    '''Loads the vadar corpus provided a path'''
+    path = args[0]
+    f = open(path,'r')
+    raw = f.read()
+    f.close()
+
+    rows = raw.split('\n')
+    #This creates a list of triples containing word, sentiment and valence
+    entries = [result.split('\t')[0:3] for result in rows]
+
+    # Subset of vadar corpus containing full length words
+    return entries[441:-17]
+
+class VadarLoader:
+
+    def __init__(self,kwargs):
+        #Set up the function to load the lexicon
+        assert 'path' in kwargs
+        
 
