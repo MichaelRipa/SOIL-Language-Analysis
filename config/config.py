5f8402082bc416bc7aab9bb6610c6913107a28d4
Michael Ripa <m.ripa123@gmail.com>
2024-05-11 12:34:07 -0400

    Added config file for loading corpus contents

---
+++ config/config.py
@@ -0,0 +1,40 @@
+#! /usr/bin/python3
+import os
+from pathlib import Path
+
+_file_dir = Path(os.path.realpath(__file__))
+repo_dir = _file_dir.parent.absolute()
+
+GERMAN_CORPUS_DIRECTORIES = [
+    os.path.join(
+        repo_dir,
+        'corpus',
+        'german',
+        year,
+        'raw'
+    ) 
+    for year in os.listdir(
+        os.path.join(
+            repo_dir
+            ,'corpus',
+            'german'
+        )
+    )
+]
+
+ITALIAN_CORPUS_DIRECTORIES = [
+    os.path.join(
+        repo_dir,
+        'corpus',
+        'italian',
+        year,
+        'raw'
+    ) 
+    for year in os.listdir(
+        os.path.join(
+            repo_dir
+            ,'corpus',
+            'italian'
+        )
+    )
+]
